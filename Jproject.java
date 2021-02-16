c9f129c117f37cc8b19c6ce796f44d1e216108b7
lyboos <72482493+lyboos@users.noreply.github.com>
2021-02-16 12:22:15 +0800

    Create Jproject.java

---
+++ Jproject.java
@@ -0,0 +1,9 @@
+public class yuan {
+    public static void main(String[] args) {
+//        System.out.println("hello LY\n");
+        System.out.println("hello LY\n");
+        System.out.println("hello LY\n");
+//        System.out.println("hello LY\n");
+        System.out.println("hello LY\n");
+    }
+}
