b346f16e4bc27c32021373ad5ecf4211b2c069ab
lyboos <72482493+lyboos@users.noreply.github.com>
2021-02-18 22:10:24 +0800

    Create Jproject2.java

---
+++ Jproject2.java
@@ -0,0 +1,24 @@
+/**
+ * 这是第一次使用文档注释
+ */
+
+public class yuan {
+    //psvm=下列语句
+    public static void main(String[] args) {
+//        System.out.println("hello LY\n");
+ /*       System.out.println("hello LY\n");
+        System.out.println("hello LY\n");*
+  */
+        //sout=下列语句
+        System.out.println('L');
+        System.out.println("hello LY");
+//        System.out.println("hello LY\n");
+        System.out.println("hello LY");
+        int a=56;
+        boolean ly=true;
+        char b='P';
+        System.out.println(b);
+        System.out.println(ly);
+        System.out.println(a);
+    }
+}
