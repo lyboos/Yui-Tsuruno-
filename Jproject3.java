c7bb5f15d8f765579942a7e775ed8a7c13d60355
lyboos <72482493+lyboos@users.noreply.github.com>
2021-02-23 23:45:08 +0800

    Create Jproject3.java

---
+++ Jproject3.java
@@ -0,0 +1,21 @@
+public class bytedemo {
+    public static void main(String[] args) {
+        byte a=1;
+        short b=2;
+        int c=4;
+        long r=88888L;
+        System.out.println(r);
+        float d=1.0F;
+        double e=3.4;
+        char z='P';
+        boolean ly=true;
+        System.out.println(ly);
+        System.out.println(b+r);
+        for (int i=0;i<4;i++) {
+            System.out.println(c+i);
+           }
+        while (ly) {
+            break;
+        }
+    }
+}
