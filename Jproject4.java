d4beb28a09519ca6f722b61da76f23e4f4dc1827
lyboos <72482493+lyboos@users.noreply.github.com>
2021-02-27 21:09:39 +0800

    Create Jproject4.java

---
+++ Jproject4.java
@@ -0,0 +1,46 @@
+//import java.util.Scanner;
+import java.util.Scanner;
+public class bytedemo {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        int a,b;
+        a=sc.nextInt();
+        b=sc.nextInt();
+        System.out.println(a+b);
+        //        Scanner sc=new Scanner(System.in);
+//        char a='a';
+//        System.out.println(a+23);
+//        System.out.println("hello"+"world");
+//        System.out.println("hello"+"world");
+//        System.out.println("hello"+"world"+10);
+//        System.out.println(10+20+"hello");
+//        int b=6;
+//        b=sc.nextInt();
+//        boolean ly=true;
+//        System.out.println(b++);
+//        System.out.println(++b);
+//        System.out.println(b);
+//        System.out.println(a!=b);
+//        System.out.println(a==b);
+//        System.out.println(b=a);
+//        System.out.println(!!!!!!!!!!ly);
+    }
+}
+
+//        byte a=1;
+//        short b=2;
+//        int c=4;
+//        long r=88888L;
+//        System.out.println(r);
+//        float d=1.0F;
+//        double e=3.4;
+//        char z='P';
+//        boolean ly=true;
+//        System.out.println(ly);
+//        System.out.println(b+r);
+//        for (int i=0;i<4;i++) {
+//            System.out.println(c+i);
+//           }
+//        while (ly) {
+//            break;
+//        }
