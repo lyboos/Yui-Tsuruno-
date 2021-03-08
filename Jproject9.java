734357acb7416b8552bbbfcd1fa65eb1076108f1
lyboos <72482493+lyboos@users.noreply.github.com>
2021-03-08 17:50:25 +0800

    Create Jproject9.java

---
+++ Jproject9.java
@@ -0,0 +1,12 @@
+public class source {
+    public static void main(String[] args) {
+        int[] arr=new int[30];
+        int brr[]={3,4,5,6,7};
+        for (int i = 0; i <30 ; i++) {
+            arr[i]=30-i;
+        }
+        for (int i = 0; i < 30; i++) {
+            System.out.print(arr[i]+" ");
+        }
+    }
+}
