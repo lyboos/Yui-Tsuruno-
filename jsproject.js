4bbfbec8bf253d040a5c1fbd297fef140bb0d01e
lyboos <72482493+lyboos@users.noreply.github.com>
2021-05-10 22:12:55 +0800

    Create jsproject.js

---
+++ jsproject.js
@@ -0,0 +1,8 @@
+alert("hello "+"world");
+console.log("hello".length);
+console.log("hello".charAt(0));
+console.log("hello world".replace("hello","goodbye"));
+console.log("hello world".toUpperCase());
+var b=true;
+console.log(1>=2)
+console.log(true && false)
